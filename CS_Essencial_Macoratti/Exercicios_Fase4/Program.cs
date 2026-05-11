dae63e30d2b52e8cfac4a1b6019181564f43ff38
Lucas Bazzani <lu.bazzani@hotmail.com>
2026-05-11 20:17:07 -0300

    Classes and Methods Exercise from 11 to 15

--- CS_Essencial_Macoratti/Exercicios_Fase4/Program.cs
+++ CS_Essencial_Macoratti/Exercicios_Fase4/Program.cs
@@ -1,30 +1,46 @@
 ﻿// 1 - O que é uma classe e qual a sua composição?
 // É um tipo estruturado com membros (Atributos e Métodos). Representa uma abstração de uma entidade do mundo real.
 
-// 2- O que é um objeto e como pode ser criado?
+// 2 - O que é um objeto e como pode ser criado?
 // Uma instância de uma classe alocada na memória Heap. Um objeto pode ser criado com o uso da palavra reservada 'new'.
 
-// 3- Qual a diferença entre classe e objeto?
+// 3 - Qual a diferença entre classe e objeto?
 // Classe é um tipo estruturado e o objeto é a materialização da classe.
 
-// 4- O que é um método e como pode ser criado e chamado?
+// 4 - O que é um método e como pode ser criado e chamado?
 // Um método é um bloco de código que especifica um comportamento de uma classe.
 
-// 5- O que é um  parâmetro de método?
+// 5 - O que é um  parâmetro de método?
 // Um valor que um método espera receber quando for chamado para utilizá-lo em sua rotina.
 
 // 6 - O que é um argumento?
 // Argumento é o valor do parâmetro do método informado na sua chamada.
 
-// 7- O que é um construtor e qual o seu objetivo?
+// 7 - O que é um construtor e qual o seu objetivo?
 // É um método com o mesmo nome da classe utilizado para inicialização de sua instância.
 
-// 8- Quais os usos da palavra chave this?
+// 8 - Quais os usos da palavra chave this?
 //Refere-se à instância atual da classe e pode ser usada também para passar um objeto da instância atual
 //para outros métodos.
 
-// 9- O que é um método com retorno? 
+// 9 - O que é um método com retorno? 
 // Um método que retorna algum valor para a classe que o chamou através da palavra reservada 'return'.
 
-// 10- O que é sobrecarga de método?
+// 10 - O que é sobrecarga de método?
 // Quando há métodos com o mesmo nome na mesma classe, mas com assinaturas diferentes.
+
+// 11 - Como  é feita  a passagem de parâmetros por valor?
+// O método recebe uma cópia do valor passado.
+
+// 12 - Como  é feita  a passagem de parâmetros por referência?
+// O método recebe uma cópia da referência passada.
+
+// 13 - Qual a diferença entre os parâmetros out e o parâmetro ref?
+// Parâmetro Ref precisa ser inicializado.
+// Parâmetro Out não precisa ser inicializado.
+
+// 14 - O que são argumentos nomeados?
+// Permite alterar a ordem dos parâmetros na chamada nomeando-os.
+
+// 15 - O que são parâmetros opcionais? 
+// Define-se um valor default para o parâmetro tonando a passagem de argumento deste parâmetro opcional.
